01f5f250c4340506fe202a78ef072dcfad7862b1
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-12-02 17:27:16 +0300

    Настройка навигации

--- app/src/main/java/com/example/androidnativetodo/MainActivity.kt
+++ app/src/main/java/com/example/androidnativetodo/MainActivity.kt
@@ -10,37 +10,48 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.androidnativetodo.ui.theme.AndroidNativeTodoTheme
+import com.example.androidnativetodo.ui.todo_edit.TodoEditScreen
+import com.example.androidnativetodo.ui.todo_list.TodoListScreen
+import com.example.androidnativetodo.utils.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AndroidNativeTodoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      AndroidNativeTodoTheme {
+        val navController = rememberNavController()
+        NavHost(
+          navController = navController,
+          startDestination = Routes.TODO_LIST
+        ) {
+          composable(Routes.TODO_LIST) {
+            TodoListScreen(onNavigate = {
+              navController.navigate(it.route)
+            })
+          }
+          composable(
+            route = Routes.TODO_EDIT + "?todoId={todoId}",
+            arguments = listOf(
+              navArgument(name = "todoId") {
+                type = NavType.IntType
+                defaultValue = -1
+              }
+            )
+            ) {
+            TodoEditScreen(onPopBackStack = {
+              navController.popBackStack()
+            })
+          }
         }
+      }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidNativeTodoTheme {
-        Greeting("Android")
-    }
+  }
 }
